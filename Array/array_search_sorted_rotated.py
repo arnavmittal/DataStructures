857202da3501abf6f6de98ce35d3b7104a6da1e0
Arnav Mittal <arnavmittal@Arnavs-MacBook-Pro-2.local>
2019-12-07 22:59:53 -0800

    Added array_search_sorted_rotated

--- Array/array_search_sorted_rotated.py
+++ Array/array_search_sorted_rotated.py
@@ -4,11 +4,13 @@
     [70,71,99,2,12] d=1 => -1 | d=2 => 3 | d=12 => 4 | d=70 => 0 | d=75 => -1 | d=99 => 2 | d=101 => -1
 
     Binary Search
-    
+    - Check pivot value against leftmost/righmost indexs' value of subarray => sorted half of the array
+    - ensure d >= min and d < max of the sorted half before looking in unsorted half of the array
+
     Time complexity - O(log(N))
     Space complexity - O(1)
 """
-from Array.array_rotation import rotateLeft
+from array_rotation import rotateLeft
 
 def sortedRotatedSearch(array, d):
     left = 0
@@ -22,11 +24,11 @@ def sortedRotatedSearch(array, d):
         elif array[mid] > array[left] and d < array[mid] and d >= array[left]:
             right = mid
         else:
-            left = mid
+            left = mid+1
     
     return -1
 
 
 if __name__ == "__main__":
     for i in [1, 2, 12, 70, 71, 75, 99, 101]:
-        print("Index of {} is {}".format(i, sortedRotatedSearch([2,12,70,71,99], i)))
+        print("Index of {} is {}".format(i, sortedRotatedSearch(rotateLeft([2,12,70,71,99], 2), i)))
